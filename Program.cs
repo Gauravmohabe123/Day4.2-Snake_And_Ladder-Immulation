942b022028cabba78c78949aacf449583cec5370
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-31 15:25:19 +0530

    Added Assignment Problem

--- Program.cs
+++ Program.cs
@@ -12,6 +12,23 @@ namespace Snake_And_Ladder
             Random random= new Random();
             int dice = random.Next(1, 7);
             Console.WriteLine(dice);
+            int option=random.Next(0,3);
+            switch(option)
+            {
+                case 0:
+                    Console.WriteLine("No play player stayed in the same position");
+                    position = position + 0;
+                    break;
+                    case 1:
+                    Console.WriteLine("The player go ahead");
+                    position = position + 1;
+                    break;
+                    case 2:
+                    Console.WriteLine("The player go back");
+                    position = position-1;
+                    break;  
+
+            }
         }
     }
 }
